7c19eb7cb4bf822a84f2771787166214b47f43e7
awwong1 <alexanderwwong@gmail.com>
2014-01-14 08:12:35 -0700

    Added sleep and not-free-tests to runner.sh script

--- runner.sh
+++ runner.sh
@@ -2,6 +2,11 @@
 # Run the webserver, run the tests and kill the webserver!
 python server.py &
 ID=$!
+
+# Sleep is necessary to setup the server
+sleep 0.5
+
 python freetests.py
+python not-free-tests.py
 kill $ID
 #pkill -P $$
